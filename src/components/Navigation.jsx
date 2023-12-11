e14d264f8ef53e4d522a70320a2865569bee4484
Valentina Muñoz <valentinamr@gmail.com>
2023-12-11 18:46:23 -0300

    Se corregir className

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,9 +5,9 @@ const Navigation = () => {
   const setActiveclassName = ({ isActive }) => (isActive ? "active" : undefined)
 
   return (
-    <>
+
       <nav className="navbar navbar-expand-lg navbar-dark bg-success bg-gradient">
-        <div class="container-fluid">
+        <div className="container-fluid">
           <a className="navbar-brand ps-3" href="#"> <h1><Image src="logo.png" />NaturalPic</h1></a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
@@ -15,20 +15,19 @@ const Navigation = () => {
           <div className="collapse navbar-collapse">
             <NavLink
               to="/"
-              classNameName={setActiveclassName}
+              classNameName={setActiveclass}
               id="NavLink"
             > Inicio
             </NavLink>
             <NavLink
               to="/favoritos"
-              classNameName={setActiveclassName}
+              classNameName={setActiveclass}
               id="NavLink"
             > Favoritos
             </NavLink>
           </div>
         </div>
       </nav>
-    </>
   )
 }
 export default Navigation
