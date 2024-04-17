a685cb260af4780afcb62f57b378144784405a9c
Valentina <valentinamr@gmail.com>
2024-04-17 12:58:24 -0400

    Update copyright

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,15 @@
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
-    <div className="bg-success bg-gradient p-3 text-white">NaturalPic - Todos los derechos reservados</div>
+    <footer className="bg-success bg-gradient p-4 text-white d-flex flex-row justify-content-between">
+      <p>© {currentYear} Todos los derechos reservados</p>
+        <p>
+          Sitio creado con <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer"> ReactJS</a> y <a href="https://getbootstrap..com/" target="_blank" rel="noopener noreferrer"
+          >Bootstrap</a>
+        </p>
+    </footer>
   )
 }
 
